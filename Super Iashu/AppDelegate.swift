afbb6fe1230ddf2b183d41ae2f4820d56b14e721
Alexandru Rosianu <me@aluxian.com>
2018-09-05 20:29:02 +0300

    Initial Commit

---
+++ Super Iashu/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Super Iashu
+//
+//  Created by Alexandru Rosianu on 05/09/2018.
+//  Copyright © 2018 AR. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
